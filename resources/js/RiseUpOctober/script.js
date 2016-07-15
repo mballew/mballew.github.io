8f72b9afd44223a13fe3bac3915bdf569b437b44
Matthew Ballew <Matthew Ballew>
2016-07-15 16:14:22 -0500

    Repair Javascript functionality

--- resources/js/RiseUpOctober/script.js
+++ resources/js/RiseUpOctober/script.js
@@ -1,3 +1,107 @@
+var i = 0;
+var filename;
+var barMapping = new Object();
+var svgRan = "false";
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    //.scale(y)
+    .orient("left");
+     //.ticks(1);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Number of Tweets:</strong> <span style='color:#FF8A65'>" + d.tweetvolume + "</span>";
+  });
+
+var svg = d3.select("#chartDiv").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.call(tip);
+
+function runTSV(){
+
+  var a = 1;
+  d3.tsv(filename, type, function(error, data) {
+    if (error) throw error;
+
+    x.domain(data.map(function(d) { return d.hour; }));
+    y.domain([0, d3.max(data, function(d) { return d.tweetvolume; })]);
+
+    svg.append("g")
+        .attr("id",function(){return"gx"})
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+          .attr("x",900)
+          .attr("y",20)
+          .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Hour of Day");
+
+    svg.append("g")
+        .attr("id",function(){return "gy"})
+        .attr("class", "y axis")
+        .attr("transform", "rotate(0)")
+        .attr("x", 45)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("id", function(d){i = i + 1; return "bar" + i })
+        .attr("x", function(d) { return x(d.hour); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.tweetvolume);})
+        .attr("onclick", "clickBar(this.id)")
+        .attr("height", function(d) { return height - y(d.tweetvolume); })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+  });
+  svgRan = "true";
+}
+
+function updateTSV(){
+  d3.tsv(filename, type, function(error,data){
+    x.domain(data.map(function(d) { return d.hour; }));
+    y.domain([0, d3.max(data, function(d) { return d.tweetvolume; })]);
+
+  svg.selectAll(".bar")
+    .data(data)
+    .attr("y",function(d){return y(d.tweetvolume);})
+    .attr("height", function(d){return height - y(d.tweetvolume); })
+    .call(yAxis);
+  });
+}
+
+function type(d) {
+  d.tweetvolume = +d.tweetvolume;
+  return d;
+}
+
+
+
 
 var text;
 var lines = [];
